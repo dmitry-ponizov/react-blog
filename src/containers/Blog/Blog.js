500d4d5ffc41757404b7bc3fbfa091b185b252fc
Your Name <admin@localhost.com>
2018-09-16 20:29:47 +0300

    add routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts'
-import { Route, NavLink, Switch } from 'react-router-dom'
-import NewPost from './NewPost/NewPost'
-import FullPost from '../Blog/FullPost/FullPost'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
+import asyncComponent from '../../hoc/asyncComponent'
+// import NewPost from './NewPost/NewPost'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 
 class Blog extends Component {
 
+    state = {
+        auth: true
+    }
     render () {
      
         return (
@@ -36,8 +44,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                   <Route path="/new-post"  component={NewPost} />
+                   {this.state.auth ? <Route path="/new-post"  component={AsyncNewPost} /> : null}
                    <Route path="/posts"  component={Posts} /> 
+                   <Route render={()=> <h1>Not found</h1>} />
+                   {/* <Redirect from="/" to="/posts" /> */}
+                   {/* <Route path="/"  component={Posts} />   */}
                 </Switch>
             </div>
         );
